1fa9bd3ee0214303dc5eb7c6e3ff19168a681c6c
HarishM-git <harishdeepakh@gmail.com>
2025-02-24 21:19:13 +0530

    self training

--- day6/selfTraining.java
+++ day6/selfTraining.java
@@ -4,3 +4,19 @@ import java.util.*;
 public class selfTraining {
   Scanner h=new Scanner(System.in);
 }
+//   const images = [
+//     "https://via.placeholder.com/300",
+//     "https://via.placeholder.com/400",
+//     "https://via.placeholder.com/500",
+//     "https://via.placeholder.com/600",
+//     "https://via.placeholder.com/700",
+//     "https://via.placeholder.com/800",
+//   ];
+
+//   return (
+//     <div>
+//       <h1>My Image Gallery</h1>
+//       <BentoGallery images={images} />
+//     </div>
+//   );
+// };
